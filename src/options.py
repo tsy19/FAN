d90d7d38ce911d60fcd51eaaee0b35eb3239f8fe
tsy19 <50548173+tsy19@users.noreply.github.com>
2023-04-28 01:32:03 -0700

    Update options.py

--- src/options.py
+++ src/options.py
@@ -4,43 +4,42 @@ def args_parser():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--max_epoch', type=int, default=30000,
-                        help="Maximum Epochs")
-    parser.add_argument('--patience', type=int, default=5, help="early stopping patience.")
+                        help="Maximum epochs allowed if early stopping not happen.")
+    parser.add_argument('--patience', type=int, default=5,
+                        help="Early stopping patience. The model will stop training if the loss not decrease for number of patience epochs.")
     parser.add_argument('--batch_size', type=int, default=128,
-                        help="")
+                        help="Training batch size.")
     parser.add_argument('--device', type=str, default="cuda:1",
-                       help="")
+                       help="Training device.")
     parser.add_argument('--lr', type=float, default=0.0005,
-                       help="learning rate")
-    # parser.add_argument('--load_from_disk', type=bool, default=False,
-    #                     help="")
+                       help="Training learning rate")
     parser.add_argument('--sample_ratio', type=float, default=0.5,
-                        help="sample how many data for IP.")
+                        help="Sample how much data for Integer Programming.")
     parser.add_argument('--seed', type=int, default=1, help='random seed')
 
 
     parser.add_argument('--dataset', type=str, default="adult",
-                        help="Dataset")
+                        help="Dataset. Select from adult, XX, XX")
     parser.add_argument('--attribute', type=str, default="sex",
-                        help="sensitive attribute")
+                        help="Sensitive attribute to protect. For adult, default is sex.")
     parser.add_argument('--fairness_notion', type=str, default="DP",
-                        help="")
+                        help="Fairness notion to use. Default is DP. Choose from DP, EO, EOs.")
     parser.add_argument('--epsilon', type=float, default=0.03,
-                        help="disparity")
+                        help="A fairness measurement. Disparity of the two groups.")
     parser.add_argument('--delta1', type=float, default=0.2,
-                        help="abstention rate of group1")
+                        help="Abstention rate allowed of group1")
     parser.add_argument('--delta2', type=float, default=0.2,
-                        help="abstention rate of group2")
+                        help="Abstention rate allowed of group2")
     parser.add_argument('--sigma1', type=float, default=1,
-                        help="abstention rate disparity of positive samples")
+                        help="Abstention rate disparity allowed for two groups within positive samples. Default 1 means no restriction.")
     parser.add_argument('--sigma0', type=float, default=1,
-                        help="abstention rate disparity of negative samples")
+                        help="Abstention rate disparity allowed for two groups within negative samples. Default 1 means no restriction.")
     parser.add_argument('--sigma', type=float, default=1,
-                        help="abstention rate disparity of all samples")
+                        help="Abstention rate disparity allowed for two groups. Default 1 means no restriction.")
     parser.add_argument('--eta1', type=float, default=0,
-                        help="error relaxation rate of group1")
+                        help="Error relaxation rate of group1.")
     parser.add_argument('--eta2', type=float, default=0,
-                        help="error relaxation rate of group2")
+                        help="Error relaxation rate of group2.")
 
     args = parser.parse_args()
     return args
